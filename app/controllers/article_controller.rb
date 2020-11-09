2945b6cc52df5fb3c586baa2278d7c6480dd0cc5
Samer.Alswairki <samer.alswairki@gmail.com>
2020-11-09 20:26:05 +0100

    Add articles listing feature

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -2,5 +2,9 @@ class ArticleController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+  
+  def index
+    @articles = Article.all
+  end
 
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :article, only: [:show]
+  resources :article, only: [:show, :index]
 end
