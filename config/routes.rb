86541df3dfd80a1f92637c09bce0bd8ec57c260a
Samer.Alswairki <samer.alswairki@gmail.com>
2020-11-13 09:05:08 +0100

    Adding Messaging Validation and flash messaging

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :article, only: [:show, :index]
+  resources :articles, only: [:show, :index, :new, :create]
 end
