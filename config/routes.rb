10c4f0f59088def0529dabd17c05dedff05179a2
Samer.Alswairki <samer.alswairki@gmail.com>
2020-11-14 10:54:08 +0100

    adding delete and fixing layout pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  #resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  #below is instead of above line and it means: resources provides all RESR-ful routes to Rails resources 
+  resources :articles
 end
